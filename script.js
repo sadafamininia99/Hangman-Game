3be313203694bf7de752d16f1337f8975c0045b6
= <aminicomputer96@gmail.com>
2021-06-10 12:30:19 +0430

    document.getelementbyid and start js code

---
+++ script.js
@@ -0,0 +1,7 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
