4fd1831d14500329f80f7747194aa44197470b0f
= <aminicomputer96@gmail.com>
2021-06-10 15:47:13 +0430

    one if for handel the correct wocab

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 //get elements
-const wordEl = document.getElementById('word');
-const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-button');
-const popup = document.getElementById('popup-container');
-const notification = document.getElementById('notification-container');
-const finalMessage = document.getElementById('final-message');
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
 
-
-
-
-const figureParts =document.querySelectorAll('.figure-part');
-const words =[ 'application' , 'programming' , 'interface' , 'wizard'];
+const figureParts = document.querySelectorAll(".figure-part");
+const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 // console.log(selectedWord);
-const correctLetters =['w' ,'i'  ,'z'];
- const wrongLetters =[];
+const correctLetters = ["w", "i", "z" ,"a", "r", "d"];
+const wrongLetters = [];
 
- //fun(show hidden word)
- function displayWord(){
-    wordEl.innerHTML=`
+//fun(show hidden word)
+function displayWord() {
+  wordEl.innerHTML = `
     ${selectedWord
-        .split(``)
-        .map(
-            letter =>`
+      .split(``)
+      .map(
+        (letter) => `
         <span class="letter">
-        ${correctLetters.includes(letter) ? letter  : ''}
+        ${correctLetters.includes(letter) ? letter : ""}
         </span>
         `
-        
-        )
-        .join('')}
+      )
+      .join("")}
     `;
-            console.log(wordEl.innerText);
+  const innerWord = wordEl.innerText.replace(/[ \n]/g, "");
+  
+
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		finalMessageRevealWord.innerText = '';
+		popup.style.display = 'flex';
+}
+}
 
 
-    }
- 
- displayWord();
+displayWord();
