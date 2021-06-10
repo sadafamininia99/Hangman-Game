30e577ad5fe90402743ff0109c4c2111485ea1ca
= <aminicomputer96@gmail.com>
2021-06-10 15:35:18 +0430

    function selected word has been added and shows the letters that are in the array of correct letters

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//get elements
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
 const playAgainBtn = document.getElementById('play-button');
@@ -5,3 +6,36 @@ const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
+
+
+
+const figureParts =document.querySelectorAll('.figure-part');
+const words =[ 'application' , 'programming' , 'interface' , 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+// console.log(selectedWord);
+const correctLetters =['w' ,'i'  ,'z'];
+ const wrongLetters =[];
+
+ //fun(show hidden word)
+ function displayWord(){
+    wordEl.innerHTML=`
+    ${selectedWord
+        .split(``)
+        .map(
+            letter =>`
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter  : ''}
+        </span>
+        `
+        
+        )
+        .join('')}
+    `;
+            console.log(wordEl.innerText);
+
+
+    }
+ 
+ displayWord();
